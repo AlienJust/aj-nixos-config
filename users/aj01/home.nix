50f1e891befc09148a6aeaf85b7f9ac2342fcc5b
Alexey Debelov <alienjustmail@gmail.com>
2023-10-27 22:32:09 +0500

    Better foot config

--- users/aj01/home.nix
+++ users/aj01/home.nix
@@ -323,11 +323,35 @@
     enable = true;
     settings = {
       main = {
-        font = lib.mkForce "IosevkaTerm NFM Light:size=14";
-        font-bold = lib.mkForce "IosevkaTerm NFM:size=14";
+        font = lib.mkForce "IosevkaTerm NFM Light:size=12";
+        font-bold = lib.mkForce "IosevkaTerm NFM:size=12";
       };
       mouse = {
         hide-when-typing = false;
+      };
+      colors = {
+        foreground = "ffffff";
+        background = "2e3440";
+        regular0 = "000000";
+        regular1 = "ff0000";
+        regular2 = "37dd21";
+        regular3 = "fee409";
+        regular4 = "1460d2";
+        regular5 = "ff005d";
+        regular6 = "00bbbb";
+        regular7 = "bbbbbb";
+        bright0 = "545454";
+        bright1 = "f40d17";
+        bright2 = "3bcf1d";
+        bright3 = "ecc809";
+        bright4 = "5555ff";
+        bright5 = "ff55ff";
+        bright6 = "6ae3f9";
+        bright7 = "ffffff";
+        alpha = 0.85;
+      };
+      cursor = {
+        color = "122637 f0cb09";
       };
     };
   };
