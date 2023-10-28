aea4e0108ab29434a632e15fa9118c77698319f4
Alexey Debelov <alienjustmail@gmail.com>
2023-10-29 00:13:32 +0500

    resolved

--- hosts/mixos/configuration.nix
+++ hosts/mixos/configuration.nix
@@ -135,7 +135,16 @@
     useDHCP = false;
     firewall.enable = false;
   };
-
+  networking.nameservers = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = ["~."];
+    fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+    extraConfig = ''
+      DNSOverTLS=yes
+    '';
+  };
   systemd.network = {
     enable = true;
     netdevs = {
@@ -290,6 +299,7 @@
       file
       nano
       curl
+      bind
       fd
       git
       man-pages
