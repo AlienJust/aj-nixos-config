aa156c0e4bb85854dd5ea168e33b6af5d56d4231
Alexey Debelov <alienjustmail@gmail.com>
2023-11-06 01:55:29 +0500

    Fixed exports Removed bars

--- users/aj01/applications/sway.nix
+++ users/aj01/applications/sway.nix
@@ -39,28 +39,28 @@ in {
     enable = true;
     wrapperFeatures.gtk = true;
     extraSessionCommands = with pkgs; ''
-      export QT_QPA_PLATFORM = wayland
-      export QT_WAYLAND_DISABLE_WINDOWDECORATION = "1"
-      export QT_QPA_PLATFORMTHEME = "qt5ct"
-      export QT_AUTO_SCREEN_SCALE_FACTOR = "1"
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+      export QT_QPA_PLATFORMTHEME="qt5ct"
+      export QT_AUTO_SCREEN_SCALE_FACTOR="1"
 
-      export MOZ_ENABLE_WAYLAND = "1"
-      export MOZ_DBUS_REMOTE = "1"
-      export MOZ_USE_XINPUT2 = "1"
-      export MOZ_WEBRENDER = "1"
-      export MOZ_ACCELERATED = "1"
+      export MOZ_ENABLE_WAYLAND="1"
+      export MOZ_DBUS_REMOTE="1"
+      export MOZ_USE_XINPUT2="1"
+      export MOZ_WEBRENDER="1"
+      export MOZ_ACCELERATED="1"
 
-      export XDG_SESSION_TYPE = "x11"
-      export XDG_CURRENT_DESKTOP = "sway"
-      export XCURSOR_SIZE = "32"
-      export XCURSOR_THEME = "Adwaita"
+      export XDG_SESSION_TYPE="x11"
+      export XDG_CURRENT_DESKTOP="sway"
+      export XCURSOR_SIZE="32"
+      export XCURSOR_THEME="Adwaita"
 
-      export NIXOS_OZONE_WL = "1"
+      export NIXOS_OZONE_WL="1"
 
-      export CLUTTER_BACKEND = "wayland"
-      export SDL_VIDEODRIVER = "wayland"
-      export _JAVA_AWT_WM_NONREPARENTING = "1"
-      export GDK_BACKEND = "wayland"
+      export CLUTTER_BACKEND="wayland"
+      export SDL_VIDEODRIVER="wayland"
+      export _JAVA_AWT_WM_NONREPARENTING="1"
+      export GDK_BACKEND="wayland"
     '';
     #      export MC_SKIN=$HOME/.config/mc/selenized.ini;
     #      export XDG_DATA_DIRS="${gnome.adwaita-icon-theme}/share:$XDG_DATA_DIRS";
@@ -361,7 +361,8 @@ in {
       # TODO: floating criteria
       # floating = {criteria = [{class = "SpeedCrunch";}];};
 
-      bars = [{command = "waybar";}];
+      # bars = [{command = "waybar";}];
+      bars = [];
     };
   };
 }
