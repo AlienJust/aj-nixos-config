c680b96516d05cc6d072472f76fd2d205b1e46f3
Alexey Debelov <alienjustmail@gmail.com>
2024-01-19 08:14:59 +0500

    Added trusted users (wheel group)

--- flake.nix
+++ flake.nix
@@ -12,6 +12,8 @@
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
+    trusted-users = ["@wheel"];
+
     extra-substituters = [
       # Nix community's cache server
       "https://nix-community.cachix.org"
