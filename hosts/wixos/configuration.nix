702d06bb4cf2b653a59ef1a4be1e70d4a4d6b097
Alexey Debelov <alienjustmail@gmail.com>
2024-02-19 09:01:01 +0500

    Added kvm group for user Also Added android-tools

--- hosts/wixos/configuration.nix
+++ hosts/wixos/configuration.nix
@@ -323,7 +323,7 @@
     description = "Alex Deb";
     shell = pkgs.zsh;
     group = "users";
-    extraGroups = ["wheel" "video" "audio" "realtime" "input" "qemu-libvirtd" "libvirtd" "adbusers" "dialout"];
+    extraGroups = ["wheel" "video" "audio" "realtime" "input" "qemu-libvirtd" "libvirtd" "kvm" "adbusers" "dialout"];
     hashedPassword = "$6$1gwYNpV/QLfIgPn5$ITN4dMnTAq78kWMthv/SJoeuoWKUmzVIqbNHFFo.CrhWrCR5qnLniOBKdzfc9Mb/qH60EeG7/CcYi/6os5lJJ/";
   };
 
@@ -399,6 +399,7 @@
       win-spice
 
       android-studio
+      android-tools
     ];
 
     persistence."/nix/persist" = {
