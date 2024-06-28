7b931a9d51eca597725b2c83072863d4fc2fabab
Alexey Debelov <alienjustmail@gmail.com>
2024-06-28 11:13:16 +0500

    Better wlogout

--- users/aj01/home.nix
+++ users/aj01/home.nix
@@ -238,7 +238,6 @@
       eww-wayland
       wl-clipboard
       hyprpicker
-      wlogout
       playerctl
       waybar
       /*
@@ -443,6 +442,54 @@
   # fzf
   programs.fzf = {enable = true;};
 
+  programs.wlogout = {
+    enable = true;
+
+    # Configuration
+    layout = [
+      {
+        label = "lock";
+        action = "swaylock";
+        text = "Lock";
+        keybind = "l";
+      }
+      {
+        label = "logout";
+        #action = "hyprctl dispatch exit 0";
+        action = "loginctl terminate-user $USER";
+        text = "Logout";
+        keybind = "e";
+      }
+      /*
+        {
+        label = "suspend";
+        action = "systemctl suspend && swaylock";
+        text = "Suspend";
+        keybind = "u";
+      }
+      */
+      {
+        label = "shutdown";
+        action = "systemctl poweroff";
+        text = "Shutdown";
+        keybind = "s";
+      }
+      /*
+        {
+        label = "hibernate";
+        action = "systemctl hibernate";
+        text = "Hibernate";
+        keybind = "h";
+      }
+      */
+      {
+        label = "reboot";
+        action = "systemctl reboot";
+        text = "Reboot";
+        keybind = "r";
+      }
+    ];
+  };
   /*
   programs.thefuck = {
     enable = true;
