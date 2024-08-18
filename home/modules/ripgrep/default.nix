bb53c0c88fe6568f8f83e67b54c3ae81c0258571
Alexey Debelov <alienjustmail@gmail.com>
2024-08-18 16:48:37 +0500

    More home modules

---
+++ home/modules/ripgrep/default.nix
@@ -0,0 +1,18 @@
+{
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.module.ripgrep;
+in {
+  options = {
+    module.ripgrep.enable = mkEnableOption "Enables ripgrep";
+  };
+
+  config = mkIf cfg.enable {
+    programs.ripgrep = {
+      enable = true;
+    };
+  };
+}
