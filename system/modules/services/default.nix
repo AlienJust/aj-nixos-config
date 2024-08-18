6c03e67d8a00c22970ec1c4d8655c3f791817856
Alexey Debelov <alienjustmail@gmail.com>
2024-08-18 16:31:56 +0500

    Added services

--- system/modules/services/default.nix
+++ system/modules/services/default.nix
@@ -2,19 +2,19 @@
   hostServicesModulesPath = "${hostModules}/services";
 in {
   imports = [
-    #"${hostServicesModulesPath}/cpu-autofreq"
-    #"${hostServicesModulesPath}/greetd-tui"
-    #"${hostServicesModulesPath}/syncthing"
-    #"${hostServicesModulesPath}/hyprland"
-    #"${hostServicesModulesPath}/printing"
-    #"${hostServicesModulesPath}/xserver"
+    "${hostServicesModulesPath}/cpu-autofreq"
+    "${hostServicesModulesPath}/greetd-tui"
+    "${hostServicesModulesPath}/syncthing"
+    "${hostServicesModulesPath}/hyprland"
+    "${hostServicesModulesPath}/printing"
+    "${hostServicesModulesPath}/xserver"
     "${hostServicesModulesPath}/polkit"
-    #"${hostServicesModulesPath}/ollama"
-    #"${hostServicesModulesPath}/fwupd"
+    "${hostServicesModulesPath}/ollama"
+    "${hostServicesModulesPath}/fwupd"
     "${hostServicesModulesPath}/udev"
-    #"${hostServicesModulesPath}/bolt"
-    #"${hostServicesModulesPath}/zram"
-    #"${hostServicesModulesPath}/tlp"
-    #"${hostServicesModulesPath}/qmk"
+    "${hostServicesModulesPath}/bolt"
+    "${hostServicesModulesPath}/zram"
+    "${hostServicesModulesPath}/tlp"
+    "${hostServicesModulesPath}/qmk"
   ];
 }
---
+++ system/modules/services/printing/default.nix
@@ -0,0 +1,16 @@
+{
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.module.services.printing;
+in {
+  options = {
+    module.services.printing.enable = mkEnableOption "Enable printing";
+  };
+
+  config = mkIf cfg.enable {
+    services.printing.enable = true;
+  };
+}
