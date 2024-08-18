8e022dcb6bb3c09aab6d19de3202a7ee77b7aaa8
Alexey Debelov <alienjustmail@gmail.com>
2024-08-18 16:10:00 +0500

    Added missing xdg-portal

---
+++ system/modules/programs/xdg-portals/default.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.module.programs.xdg-portal;
+in {
+  options = {
+    module.programs.xdg-portal.enable = mkEnableOption "Enable xdg-portal";
+  };
+
+  config = mkIf cfg.enable {
+    xdg.portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+
+      config = {
+        common = {
+          default = "*";
+
+          #"org.freedesktop.impl.portal.Screencast" = "hyprland";
+          #"org.freedesktop.impl.portal.Screenshot" = "hyprland";
+
+          "org.freedesktop.impl.portal.Screencast" = "sway";
+          "org.freedesktop.impl.portal.Screenshot" = "sway";
+        };
+      };
+
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-gnome
+      ];
+    };
+  };
+}
