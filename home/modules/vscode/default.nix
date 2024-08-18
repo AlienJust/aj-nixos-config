d69a3f214dd4bce74ca4bae72d21aa267869e6ea
Alexey Debelov <alienjustmail@gmail.com>
2024-08-18 16:59:26 +0500

    More programs

---
+++ home/modules/vscode/default.nix
@@ -0,0 +1,118 @@
+{
+  lib,
+  config,
+  homeModules,
+  ...
+}:
+with lib; let
+  cfg = config.module.vscode;
+in {
+  imports = [
+    #"${homeModules}/vscode/keybindings"
+    #"${homeModules}/vscode/extentions"
+    #"${homeModules}/vscode/settings"
+  ];
+
+  options = {
+    module.vscode.enable = mkEnableOption "Enables vscode";
+  };
+
+  config = mkIf cfg.enable {
+    /*
+    programs.vscode = {
+      enable = true;
+      enableUpdateCheck = false;
+    };
+    */
+    programs.vscode = {
+      enable = true;
+      enableUpdateCheck = false;
+
+      package = pkgs.vscode.fhsWithPackages (ps:
+        with ps; [
+          # needed for rust lang server and rust-analyzer extension
+          #rustup
+          /*
+          zlib
+          openssl.dev
+          pkg-config
+          # for dotnet
+          (with dotnetCorePackages;
+            combinePackages [
+              sdk_6_0
+              sdk_7_0
+              sdk_8_0
+            ])
+          # direnv
+          direnv
+          */
+        ]);
+      extensions =
+        with pkgs.vscode-extensions; [
+          #mvllow.rose-pine
+          rust-lang.rust-analyzer
+          eamodio.gitlens
+          pkief.material-product-icons
+          pkief.material-icon-theme
+          jnoortheen.nix-ide
+          ms-dotnettools.csharp
+          ms-dotnettools.csdevkit
+          # pflannery.vscode-versionlens
+        ]
+        # https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csdevkit
+        # a308e33f116c319578657700b02c8c69f975db4212d5f1e8ca9acd4db93d3eda  /nix/store/ri7yc2gfjwz1sjh8z4djins1jcqnik6f-ms-dotnettools-csdevkit.zip
+        /*
+        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "csdevkit";
+            publisher = "ms-dotnettools";
+            version = "1.3.2";
+            sha256 = "a308e33f116c319578657700b02c8c69f975db4212d5f1e8ca9acd4db93d3eda";
+            sourceRoot = ".";
+          }
+        ]
+        */
+        ;
+      userSettings = {
+        #"editor.fontSize" = 16;
+        "editor.fontLigatures" = true;
+        "editor.formatOnSave" = true;
+        #"editor.fontFamily" = "'Iosevka Nerd Font', 'Noto Color Emoji', 'monospace', monospace";
+        "editor.wordWrap" = true;
+        #"workbench.colorTheme" = "Rosé Pine Moon";
+        "workbench.iconTheme" = "material-icon-theme";
+        "workbench.startupEditor" = "none";
+        "workbench.productIconTheme" = "material-product-icons";
+        "git.confirmSync" = false;
+        "git.autofetch" = true;
+        "git.enableSmartCommit" = true;
+        "window.titleBarStyle" = "native";
+        "window.menuBarVisibility" = "toggle";
+        "window.zoomLevel" = 0;
+        "[nix]" = {"editor.defaultFormatter" = "jnoortheen.nix-ide";};
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nil";
+        "nix.serverSettings" = {
+          "nil" = {
+            "formatting" = {
+              "command" = ["alejandra"];
+            };
+          };
+        };
+
+        "omnisharp.enableAsyncCompletion" = true;
+        "omnisharp.enableLspDriver" = true;
+        "omnisharp.organizeImportsOnFormat" = true;
+        "csharp.inlayHints.enableInlayHintsForImplicitVariableTypes" = true;
+        "csharp.inlayHints.enableInlayHintsForImplicitObjectCreation" = true;
+        "csharp.inlayHints.enableInlayHintsForLambdaParameterTypes" = true;
+        "csharp.inlayHints.enableInlayHintsForTypes" = true;
+        "dotnet.inlayHints.enableInlayHintsForIndexerParameters" = true;
+        "dotnet.inlayHints.enableInlayHintsForLiteralParameters" = true;
+        "dotnet.inlayHints.enableInlayHintsForObjectCreationParameters" = true;
+        "dotnet.inlayHints.enableInlayHintsForOtherParameters" = true;
+        "dotnet.inlayHints.enableInlayHintsForParameters" = true;
+      };
+    };
+  };
+}
---
+++ home/modules/zathura/default.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.module.zathura;
+in {
+  options = {
+    module.zathura.enable = mkEnableOption "Enables zathura";
+  };
+
+  config = mkIf cfg.enable {
+    programs.zathura = {
+      enable = true;
+    };
+  };
+}
