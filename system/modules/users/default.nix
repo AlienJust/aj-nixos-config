aff4f68a5e5471558738d5cb3ff86855a6368664
Alexey Debelov <alienjustmail@gmail.com>
2024-08-20 21:56:59 +0500

    Changed default shell Changed extra groups

--- system/modules/users/default.nix
+++ system/modules/users/default.nix
@@ -22,16 +22,25 @@ in {
           isNormalUser = true;
           description = "${username}";
           home = "/home/${username}";
-          shell = pkgs.fish;
+          #shell = pkgs.fish;
+          shell = pkgs.zsh;
           #hashedPasswordFile = "${self}/secrets/hashes/${username}.hash";
           hashedPassword = "$6$1gwYNpV/QLfIgPn5$ITN4dMnTAq78kWMthv/SJoeuoWKUmzVIqbNHFFo.CrhWrCR5qnLniOBKdzfc9Mb/qH60EeG7/CcYi/6os5lJJ/"; # TODO: replace with hashedPasswordFile
 
           extraGroups = [
             "audio"
-            "networkmanager"
+            #"networkmanager"
             "wheel"
             "docker"
+            "video"
+            "realtime"
+            "input"
+            "qemu-libvirtd"
             "libvirtd"
+            "kvm"
+
+            "adbusers"
+            #"acme"
           ];
         };
 
