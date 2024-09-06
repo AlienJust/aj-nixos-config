c880f7507b6c3c8316f3640cb20077df9e7759b3
Alexey Debelov <alienjustmail@gmail.com>
2024-09-06 16:11:48 +0500

    Misc changes

--- home/modules/wlogout/default.nix
+++ home/modules/wlogout/default.nix
@@ -12,54 +12,53 @@ in {
 
   config = mkIf cfg.enable {
     programs.wlogout = {
-    enable = true;
+      enable = true;
 
-    # Configuration
-    layout = [
-      {
-        label = "lock";
-        action = "swaylock";
-        text = "Lock";
-        keybind = "l";
-      }
-      {
-        label = "logout";
-        #action = "hyprctl dispatch exit 0";
-        action = "loginctl terminate-user $USER";
-        text = "Logout";
-        keybind = "e";
-      }
-      /*
+      # Configuration
+      layout = [
         {
-        label = "suspend";
-        action = "systemctl suspend && swaylock";
-        text = "Suspend";
-        keybind = "u";
-      }
-      */
-      {
-        label = "shutdown";
-        action = "systemctl poweroff";
-        text = "Shutdown";
-        keybind = "s";
-      }
-      /*
+          label = "lock";
+          action = "swaylock";
+          #action = "hyprlock";
+          text = "Lock";
+          keybind = "l";
+        }
         {
-        label = "hibernate";
-        action = "systemctl hibernate";
-        text = "Hibernate";
-        keybind = "h";
-      }
-      */
-      {
-        label = "reboot";
-        action = "systemctl reboot";
-        text = "Reboot";
-        keybind = "r";
-      }
-    ];
-  };
+          label = "logout";
+          #action = "hyprctl dispatch exit 0";
+          action = "loginctl terminate-user $USER";
+          text = "Logout";
+          keybind = "e";
+        }
+        /*
+          {
+          label = "suspend";
+          action = "systemctl suspend && swaylock";
+          text = "Suspend";
+          keybind = "u";
+        }
+        */
+        {
+          label = "shutdown";
+          action = "systemctl poweroff";
+          text = "Shutdown";
+          keybind = "s";
+        }
+        /*
+          {
+          label = "hibernate";
+          action = "systemctl hibernate";
+          text = "Hibernate";
+          keybind = "h";
+        }
+        */
+        {
+          label = "reboot";
+          action = "systemctl reboot";
+          text = "Reboot";
+          keybind = "r";
+        }
+      ];
+    };
   };
 }
-
-
