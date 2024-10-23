1b7f5667ed010fb3cd0e07d25bf7f20feb7686e7
Alexey Debelov <alienjustmail@gmail.com>
2024-10-23 10:52:03 +0500

    Add hpb package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,5 @@
 {pkgs, ...}: {
   # mplus-fonts = pkgs.callPackage ./mplus-fonts { };
   spoofdpi = pkgs.callPackage ./spoofdpi {};
+  hpb = pkgs.callPackage ./hpb {};
 }
---
+++ pkgs/hpb/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchgit,
+}:
+stdenvNoCC.mkDerivation rec {
+  pname = "hpb";
+  version = "1.0";
+  dontConfigue = true;
+
+  src = fetchgit {
+    url = "https://192.168.11.20:50589/sitandra/hpb";
+    rev = "d959c04f8c58d3e2d5b3a44ba9077c42b67ec160";
+    sha256 = "sha256-MlqJOoMSRuYeG+jl8DFgcNnpEyeRgDCK2JlN9pOqBWA=";
+  };
+
+  installPhase = ''
+    runHook preInstall
+
+    rm -rf .dump
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Horizon protocol builder";
+    homepage = "https://192.168.11.20:50589/sitandra/hpb";
+    #maintainers = with maintainers; [gyaru];
+    platforms = platforms.all;
+    license = licenses.ofl;
+  };
+}
