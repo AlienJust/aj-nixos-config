40ba58e324ab175d936303070c0d0a9fdf93644a
Alexey Debelov <alienjustmail@gmail.com>
2024-11-03 03:29:58 +0500

    Format

--- system/modules/games/default.nix
+++ system/modules/games/default.nix
@@ -13,25 +13,21 @@ in {
 
   config = mkIf cfg.enable {
     programs.steam = {
-    enable = true;
-    gamescopeSession.enable = false;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    /*
-    package = pkgs.steam.override {
-      extraLibraries = p:
-        with p; [
-          (lib.getLib xwayland)
-          (lib.getLib dconf)
-          (lib.getLib gvfs)
-        ];
+      enable = true;
+      gamescopeSession.enable = false;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      /*
+      package = pkgs.steam.override {
+        extraLibraries = p:
+          with p; [
+            (lib.getLib xwayland)
+            (lib.getLib dconf)
+            (lib.getLib gvfs)
+          ];
+      };
+      */
     };
-    */
-  };
-  programs.gamemode.enable = true;
+    programs.gamemode.enable = true;
   };
 }
-
-
-
-
