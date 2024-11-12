102827f63a63831f412b41bd00b472253fd6be64
Alexey Debelov <alienjustmail@gmail.com>
2024-11-12 14:55:24 +0500

    Fixed zapret

--- system/modules/services/zapret/default.nix
+++ system/modules/services/zapret/default.nix
@@ -14,7 +14,9 @@ in {
 
   config = mkIf cfg.enable {
     services.zapret = {
+      /*
       enable = true;
+
       package = (
         pkgs.zapret.overrideAttrs (
           finalAttrs: previousAttrs: {
@@ -27,6 +29,7 @@ in {
           }
         )
       );
+
       params = [
         #"--dpi-desync=disorder --dpi-desync-ttl=1 --dpi-desync-split-pos=3"
         #"--dpi-desync=fake --dpi-desync-repeats=6"
@@ -37,6 +40,60 @@ in {
         #конкретно это мой провайдер, с этими параметрами работает у меня либо все, либо почти все.
         #"--dpi-desync=split2 --dpi-desync=fake --dpi-desync-repeats=6 " # youtube
         #"--dpi-desync-any-protocol --dpi-desync=fake" # discord
+      ];
+      */
+
+      enable = true;
+      params = [
+        "--dpi-desync-autottl=3"
+        "--wssize 1:6"
+        "--dpi-desync-fake-tls=0x00000000"
+        "--dpi-desync-split-pos=1"
+        "--dpi-desync=syndata,fake,split2"
+        "--dpi-desync-repeats=6"
+        "--dpi-desync-fooling=md5sig"
+        "--new"
+      ];
+      whitelist = [
+        "googlevideo.com"
+        "youtu.be"
+        "youtube.com"
+        "youtubei.googleapis.com"
+        "googlevideo.com"
+        "youtu.be"
+        "youtube.com"
+        "youtubei.googleapis.com"
+        "youtubeembeddedplayer.googleapis.com"
+        "ytimg.l.google.com"
+        "ytimg.com"
+        "jnn-pa.googleapis.com"
+        "youtube-nocookie.com"
+        "youtube-ui.l.google.com"
+        "yt-video-upload.l.google.com"
+        "wide-youtube.l.google.com"
+        "youtubekids.com"
+        "ggpht.com"
+        "discord.com"
+        "gateway.discord.gg"
+        "cdn.discordapp.com"
+        "discordapp.net"
+        "discordapp.com"
+        "discord.gg"
+        "media.discordapp.net"
+        "images-ext-1.discordapp.net"
+        "discord.app"
+        "discord.media"
+        "discordcdn.com"
+        "discord.dev"
+        "discord.new"
+        "discord.gift"
+        "discordstatus.com"
+        "dis.gd"
+        "discord.co"
+        "discord-attachments-uploads-prd.storage.googleapis.com"
+        "7tv.app"
+        "7tv.io"
+        "10tv.app"
       ];
     };
   };
