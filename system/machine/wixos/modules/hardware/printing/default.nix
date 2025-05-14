b51d74aa16c476b7310c7863f0d9f660c0e0deb1
Alexey Debelov <alienjustmail@gmail.com>
2025-05-14 13:00:59 +0500

    change device uri

--- system/machine/wixos/modules/hardware/printing/default.nix
+++ system/machine/wixos/modules/hardware/printing/default.nix
@@ -11,8 +11,8 @@
       {
         name = "hp1606dn"; # Произвольное имя принтера
         description = "HP LaserJet P1606dn";
-        deviceUri = "socket://192.168.11.20:9100"; # Или `ipp://<IP_принтера>`
-        # deviceUri = "hp:/net/HP_LaserJet_Professional_P1606dn?ip=192.168.1.50";
+        # deviceUri = "socket://192.168.11.20:9100"; # Или `ipp://<IP_принтера>`
+        deviceUri = "hp:/net/HP_LaserJet_Professional_P1606dn?ip=192.168.11.20";
         # model = "drv:///hpcups.drv/hp-laserjet_p1606dn.ppd"; # Драйвер
         # model = "drv:///hp/hpcups.drv/hp-laserjet_pro_p1606dn.ppd";
         # model = "${pkgs.hplipWithPlugin}/share/ppd/HP/hp-laserjet_p1606dn.ppd";
