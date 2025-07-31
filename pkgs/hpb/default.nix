8127ed13d7bc54d00cbe097fe45987fbd5040b5c
Alexey Debelov <alienjustmail@gmail.com>
2025-07-31 23:54:19 +0500

    Update HPB

--- pkgs/hpb/default.nix
+++ pkgs/hpb/default.nix
@@ -7,7 +7,7 @@ stdenvNoCC.mkDerivation rec {
   pname = "hpb";
   version = "1.0.4";
   dontConfigue = true;
-  src = ./hpb-1.0.4.tar.gz;
+  src = ./hpb-1.0.5.tar.gz;
   /*
   src = fetchgit {
     url = "https://192.168.11.20:50589/sitandra/hpb.git";
