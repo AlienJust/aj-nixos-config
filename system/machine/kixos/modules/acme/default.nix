01b05b48b8a17b6a6cc5889fbc9523b49bf22b07
Alexey Debelov <alienjustmail@gmail.com>
2026-02-10 15:45:00 +0500

    Change acme config

--- system/machine/kixos/modules/acme/default.nix
+++ system/machine/kixos/modules/acme/default.nix
@@ -6,6 +6,23 @@
 }: {
   ## TODO fix when lego in stable v4.20.0+
   #nixpkgs.overlays = [(_: _: {lego = inputs.unstable.legacyPackages.${pkgs.system}.lego.override {};})];
+  users.users.nginx.extraGroups = ["acme"];
+
+  services.nginx = {
+    enable = true;
+    virtualHosts = {
+      "alexdeb.ru" = {
+        # Catchall vhost, will redirect users to HTTPS for all vhosts
+        serverAliases = ["*.alexdeb.ru"];
+        locations."/.well-known/acme-challenge" = {
+          root = "/var/lib/acme/.challenges";
+        };
+        locations."/" = {
+          return = "301 https://$host$request_uri";
+        };
+      };
+    };
+  };
 
   security.acme = {
     acceptTerms = true;
@@ -17,10 +34,11 @@
 
     certs = {
       "alexdeb.ru" = {
+        webroot = "/var/lib/acme/.challenges";
         extraDomainNames = ["*.alexdeb.ru"];
         #dnsProvider = "timewebcloud";
         #credentialsFile = config.sops.secrets."dns/token".path;
-        webroot = null;
+        #webroot = null;
       };
     };
   };
