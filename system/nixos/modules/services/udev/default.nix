b078e80603c6f206a562806b478e92debb1a2329
Alexey Debelov <alienjustmail@gmail.com>
2026-03-04 13:00:19 +0500

    Add udev rule for ttyUSB

--- system/nixos/modules/services/udev/default.nix
+++ system/nixos/modules/services/udev/default.nix
@@ -14,6 +14,7 @@ in {
   config = mkIf cfg.enable {
     # Udev rules
     services.udev.extraRules = ''
+      KERNEL=="ttyUSB[0-9]*", MODE="0666"
       ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c548", ATTR{power/wakeup}="disabled"
     '';
   };
