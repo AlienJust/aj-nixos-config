dc4e9ff15884e7fafe92abf421a8108f48c715eb
Alexey Debelov <alienjustmail@gmail.com>
2026-09-09 12:09:36 +0500

    Add work wg for wixos

--- system/machine/wixos/modules/hardware/network/default.nix
+++ system/machine/wixos/modules/hardware/network/default.nix
@@ -112,5 +112,28 @@
         }
       ];
     };
+
+    wg0 = {
+      address = [
+        "10.66.66.2/32"
+
+        /*
+        "fd42:42:42::2/128"
+        */
+      ];
+      # dns = [ "10.0.0.1" "fdc9:281f:04d7:9ee9::1" ];
+      # privateKeyFile = "/home/aj01/wireguard-keys/privatekey";
+      privateKeyFile = config.sops.secrets.work_privatekey.path;
+
+      peers = [
+        {
+          publicKey = "AHK8uBAHN29XfPYJmzh/hjhOkEGuzf/HDZRayR7RlBw=";
+          presharedKeyFile = config.sops.secrets.work_presharedk.path;
+          allowedIPs = ["192.168.167.0/24" "192.168.6.0/24"];
+          endpoint = "79.172.45.20:40414";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
   };
 }
