ca3790e6a3f44abc7dd9a37e97c2a9ce4acd6d4d
Alexey Debelov <alienjustmail@gmail.com>
2026-09-09 20:28:20 +0500

    Enable wg client-to-client connection

--- system/machine/mixos/modules/hardware/network/default.nix
+++ system/machine/mixos/modules/hardware/network/default.nix
@@ -96,7 +96,7 @@
         {
           publicKey = "AHK8uBAHN29XfPYJmzh/hjhOkEGuzf/HDZRayR7RlBw=";
           presharedKeyFile = config.sops.secrets.work_presharedk.path;
-          allowedIPs = ["192.168.167.0/24" "192.168.10.0/24" "192.168.6.0/24"];
+          allowedIPs = ["10.66.66.0/24" "192.168.167.0/24" "192.168.10.0/24" "192.168.6.0/24"];
           endpoint = "79.172.45.20:40414";
           persistentKeepalive = 25;
         }
